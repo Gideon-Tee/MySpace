ad9cb3bd915592397bc718916c3c578e1f3057a2
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-09 23:25:47 +0000

    created and registered the Post model

--- MySpace/MySpace_app/models.py
+++ MySpace/MySpace_app/models.py
@@ -1,5 +1,8 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 from django.contrib.auth import get_user_model
+import uuid
 # Create your models here.
 
 User = get_user_model()
@@ -13,3 +16,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    caption = models.TextField(blank=True)
+    image = models.ImageField(upload_to='post_images', blank=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user}'
